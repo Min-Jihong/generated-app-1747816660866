f0cbf5f80420fbb55eb1b6e4fa07fedcf7c4e59c
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 17:37:47 +0900

    Add pages/_app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,3 @@
+{
+  "content": "import '../styles/globals.css';\\nimport type { AppProps } from 'next/app';\\n\\nfunction MyApp({ Component, pageProps }: AppProps) {\\n  return <Component {...pageProps} />;\\n}\\n\\nexport default MyApp;"
+}
